48358ba7232169451e88496ce5cdc0c0182abd84
LittleJustices <justym2c@gmail.com>
2022-03-31 17:05:49 +0200

    some extra flag functions for later

--- src/flagTracker.js
+++ src/flagTracker.js
@@ -4,4 +4,19 @@ class FlagTracker {
     static checkForFlag(flag) {
         return this._flags.includes(flag);
     }
+
+    static addFlag(flag) {
+        this._flags.push(flag);
+    }
+
+    static removeFlag(flag) {
+        let index = this._flags.lastIndexOf(flag);
+        if (index > -1) {
+            this._flags.splice(index, 1);
+        }
+    }
+
+    static latestFlag() {
+        return this._flags[this._flags.length - 1];
+    }
 }
