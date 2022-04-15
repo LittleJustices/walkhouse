53948ee6c7a3f6c5586fbf7024a079ecb4419eba
LittleJustices <justym2c@gmail.com>
2022-04-15 10:49:48 +0200

    remove old interaction commands

--- src/commands.js
+++ src/commands.js
@@ -29,22 +29,6 @@ class Commands {
         }
     }
 
-    static beginInteractionCommand() {
-        return function(interactionTarget) {
-            gameState.state = GameState.interactionState;
-            // locking entities besides the target and player goes here maybe
-            dialogueBox.displayDialogue("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce ultrices, mi quis lobortis auctor, velit felis bibendum nisi, ut finibus sapien mauris ut eros. Phasellus aliquam venenatis ipsum, vel scelerisque tellus. Sed rutrum tortor mattis, euismod mauris vitae, porttitor velit. Maecenas rutrum ante ante, sed iaculis neque eleifend non. Aliquam erat volutpat. Proin leo eros, interdum ornare nunc ultricies, porttitor maximus nisi. Cras ut ipsum molestie, mattis augue a, cursus eros. In faucibus augue justo, vel iaculis tellus lobortis ac.");
-
-        }
-    }
-
-    static endInteractionCommand() {
-        return function() {
-            // unlock entities that were involved in the interaction
-            gameState.state = GameState.explorationState;
-        }
-    }
-
     static openMenuCommand() {
         return function() {
             gameState.state = GameState.menuState;
