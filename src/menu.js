b2ee8b47ceb8a575e355adfc5bc13066b3eece54
LittleJustices <justym2c@gmail.com>
2022-04-16 11:33:24 +0200

    add menu parent sizer

--- src/menu.js
+++ src/menu.js
@@ -1,5 +1,24 @@
 class Menu {
     constructor(scene) {
         this.scene = scene;
+
+        this.mainPanel = this.createMainPanel(this.scene, {
+            height: 50,
+            anchor: {
+                centerX: "center",
+                top: "top+10",
+                width: "50%",
+                height: "66%",
+            }
+        });
+
+        this.mainPanel.layout();
+        this.mainPanel.drawBounds(scene.add.graphics(), 0xff0000);   // Comment out to get rid of sizer outline
+    }
+
+    createMainPanel(scene, config) {
+        var mainPanel = scene.rexUI.add.sizer(config);
+
+        return mainPanel;
     }
 }
